e6c946d17a7f6a4bb029285b38ea3640c289e7bc
Medkabs <mohamedkaba0629@gmail.com>
2025-01-31 22:00:55 -0500

    minor changes

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -45,11 +45,11 @@ const Banner = () => {
                </div>
             </div>
             <div className="hero-countdown-wrap">
-               <h2 className="hero-countdown-wrap-title">ICO will start in..</h2>
+               <h2 className="hero-countdown-wrap-title">Pre-launch will start in...</h2>
                <ul className="skill-feature_list">
                   <li><span>Value</span> of technology invested</li>
                   <li><span>Private</span> sale</li>
-                  <li><h4>ICO</h4></li>
+                  <li><h4>Pre-launch</h4></li>
                </ul>
                <div className="skill-feature">
                   <div className="progress">
