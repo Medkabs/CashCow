d396a54ab7ec5fd28c008894b21069a1350c365a
Medkabs <mohamedkaba0629@gmail.com>
2025-05-05 22:16:28 -0400

    Update Breadcrumb.tsx

--- src/components/common/Breadcrumb.tsx
+++ src/components/common/Breadcrumb.tsx
@@ -23,8 +23,8 @@ const Breadcrumb = ({ title }: any) => {
             </div>
          </div>
          <div className="breadcrumb-shape-wrap">
-            <Image src={breadcrumbShape_1} alt="" className="alltuchtopdown" id="bread" />
-            <Image src={breadcrumbShape_2} alt="" className="rotateme" />
+            <Image src={breadcrumbShape_1} alt="" className="alltuchtopdown" id="bread" unoptimized={true}/>
+            <Image src={breadcrumbShape_2} alt="" className="rotateme" unoptimized={true}/>
          </div>
       </section>
    )
